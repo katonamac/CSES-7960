92c40842c68e89e52bbb6fbb9bd88bfa7d7dc918
Kat <iamthekat@gmail.com>
2018-07-25 10:25:55 -0500

    add final project

--- CoursePageTool.py
+++ CoursePageTool.py
@@ -6,20 +6,30 @@ and then when it comes time to write the file I want to create a filename based 
 import re
 import sys
 import os
+import numpy as np
 import pandas as pd
+from bs4 import BeautifulSoup
 
 
-def getCourses(filename):
-    rawHtml = open(filename, 'r')
-    courses = re.findall(r'<th CLASS=\"ddtitle\" scope=\"colgroup\" ><a href=\".*\">(.*)</a>', rawHtml)
-    rawHtml.close()
-    return courses
+def get_soup():
+    rawHTML = open(sys.argv[1], 'r')
+    soup = BeautifulSoup(rawHTML, 'html.parser')
+    rawHTML.close()
+    return soup
+
+def get_courses(soup):
+    links = soup.find_all('a')
+    textList = []
+    for link in links:
+        text = link.get_text()
+        textList.append(text)
+    mess = '\n'.join(textList)
+    regex = re.compile(r'(.* - .*)')
+    courseList = re.findall(regex, mess)
+    return courseList
 
 
-# This function works OK, pass it the list of courses and it will split them
-# into their components to populate the dataframe
-
-def cleanCourses(courseList):
+def clean_courses(courseList):
     readyList = []
 
     for i in courseList:
@@ -27,15 +37,30 @@ def cleanCourses(courseList):
     return readyList
 
 
-def createCourseFrame(readyList):
-    testDataFrame = pd.DataFrame(readyList, columns = ["Course Title", "CRN", "Course Number", "Section"])
-    return testDataFrame
+def assemble_filename():
+    filename = str(sys.argv[2]) + str(sys.argv[3]) + str(sys.argv[4]) + '.csv'
+    return filename
 
+pageMess = get_soup()
 
-def writefile():
-    # do something
-    pass
+courseList = get_courses(pageMess)
 
-def getUserInput():
-    # do something
-    pass
+# instList = ["Carmen P Brysch", "Adam Andrew Payne", "Carmen P Brysch", "James Alan Norwood",
+#            "Carmen P Brysch", "James Alan Norwood", "James Alan Norwood", "TBA",
+#            "Adam Andrew Payne", "TBA", "Philip Lynn Chaney", "Adam Andrew Payne",
+#            "Ronald Dale Lewis", "Carmen P Brysch", "Stephanie Rose Rogers", "TBA",
+#            "TBA", "TBA", "TBA", "Chandana Mitra", "Adam Andrew Payne", "Philip Lynn Chaney",
+#            "Stephanie L. Shepherd", "Luke Jonathon Marzen", "Stephanie Rose Rogers", "Donn Andrew Rodekohr",
+#            "Christopher George Burton", "TBA", "Chandana Mitra", "Adam Andrew Payne",
+#            "Philip Lynn Chaney", "Stephanie L. Shepherd", "Luke Jonathon Marzen", "Stephanie Rose Rogers",
+#            "Donn Andrew Rodekohr", "Christopher George Burton", "TBA", "TBA", "TBA", "TBA",
+#            "TBA", "TBA", "TBA", "Christopher George Burton", "Philip Lynn Chaney", "Luke Jonathon Marzen",
+#            "Chandana Mitra", "Stephanie L. Shepherd"]
+
+courseData = pd.DataFrame(clean_courses(courseList), columns = ["Course Title", "CRN", "Course Number", "Section"])
+
+# instData = pd.DataFrame(instList, columns = ["Instructor"])
+
+# finalFrame = pd.concat([courseData, instData], axis = 1)
+
+finalTable = courseData.to_csv(assemble_filename(), header = True, index = False)
